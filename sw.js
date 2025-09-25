4f8ae03c1a4debd1a960ef12d217d12ace20d823
Belen5 <155993873+Belen5@users.noreply.github.com>
2025-09-25 16:38:05 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,59 @@
-
 // sw.js
-self.addEventListener('fetch',
-  function(evento) {
-    // http://localhost/pwasd25/pwa2/index.jpg > unicorn.jpg
-    // http://localhost/pwasd25/pwa2/index.jpeg > utp.png
-    console.log(evento.request.url);
-    if(/\.jpg$/.test(evento.request.url)) {
-      evento.respondWith(
-        fetch('corazon.jpg')
-      );
+
+var nombreCache = 'cache1';
+
+self.addEventListener(
+    'install',
+    function(evento) {
+        evento.waitUntil(
+            caches.open(nombreCache)
+            .then(
+                function(cache) {
+                    cache.addAll(
+                        [
+                            'index.html',
+                            'bootstrap-5.0.2-dist/css/bootstrap.min.css',
+                            'bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js',
+                            'lib1.js',
+                            'lib2.js',
+                            'hola.jpg',
+                            'corazon.jpg',
+                            'utp.png'
+                        ]
+                    );
+                }
+            )
+        );
     }
-    else if(/\.png$/.test(evento.request.url)) {
-      evento.respondWith(
-        fetch('42327.png')
-      );
+);
+
+self.addEventListener(
+    'fetch',
+    function(evento) {
+        // http://localhost/pwasd25/pwa2/index.jpg > unicorn.jpg
+        if (/\.jpg$/.test(evento.request.url)) {
+            evento.respondWith(
+                fetch('unicorn.jpg')
+            );
+        }
+        // http://localhost/pwasd25/pwa2/index.png > utp.png
+        else if (/\.png$/.test(evento.request.url)) {
+            evento.respondWith(
+                fetch('utp.png')
+            );
+        }
+        else {
+            evento.respondWith(
+                caches.match(evento.request)
+                .then(
+                    function(respuesta) {
+                        if (respuesta) {
+                            return respuesta;
+                        }
+                        return fetch(evento.request);
+                    }
+                )
+            );
+        }
     }
-  }
 );
